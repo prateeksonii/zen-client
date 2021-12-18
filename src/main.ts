31deb1037ec4104f0cd166ac9dccddb4b1aefd35
prateeksonii <prateek_soni@outlook.com>
2021-12-18 22:15:26 +0530

    setup routes
    Signed-off-by: prateeksonii <prateek_soni@outlook.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import 'modern-normalize';
+import './global.scss';
+// import 'normalize.css';
 
 createApp(App).use(router).mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Practice your web development skills</h1>
+    <router-link class="link" to="/dashboard">Get Started</router-link>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 });
 </script>
+
+<style lang="scss" scoped>
+.home {
+  height: 100vh;
+  background-color: rgb(24, 24, 24);
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.link {
+  text-decoration: none;
+  padding: 0.7rem 2rem;
+  border: none;
+  margin-top: 2rem;
+  border-radius: 5px;
+  font-weight: bold;
+  color: #fff;
+  background: rgb(50, 129, 57);
+  cursor: pointer;
+  transition: all 0.2s ease-out;
+
+  &:hover {
+    background: rgb(36, 92, 41);
+  }
+}
+</style>
